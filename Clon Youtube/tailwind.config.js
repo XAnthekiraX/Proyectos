225aec5c36645444f2d8458d4681a15e3261aa9d
XAnthekiraX <alexbalverde123@gmail.com>
2024-05-09 19:54:32 -0500

    Updated

---
+++ Clon Youtube/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'alto': {
+          '50': '#f7f7f7',
+          '100': '#ededed',
+          '200': '#d9d9d9',
+          '300': '#c8c8c8',
+          '400': '#adadad',
+          '500': '#999999',
+          '600': '#888888',
+          '700': '#7b7b7b',
+          '800': '#676767',
+          '900': '#545454',
+          '950': '#363636',
+        },
+        borderRadius: {
+          'rounded':{
+            
+          },
+        },
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
